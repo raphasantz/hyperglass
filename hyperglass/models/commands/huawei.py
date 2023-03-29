bad7430125cd920bd30d4477bac9362eca55fa06
Rudimar Remontti <rudiremontti@gmail.com>
2023-03-29 11:20:44 -0300

    Update huawei.py

--- hyperglass/models/commands/huawei.py
+++ hyperglass/models/commands/huawei.py
@@ -13,7 +13,7 @@ class _IPv4(CommandSet):
     bgp_community: StrictStr = "display bgp routing-table community {target} | no-more"
     bgp_aspath: StrictStr = "display bgp routing-table regular-expression {target}"
     bgp_route: StrictStr = "display bgp routing-table {target} | no-more"
-    ping: StrictStr = "ping -c 4 -a {source} {target}"
+    ping: StrictStr = "ping -c 5 -a {source} {target}"
     traceroute: StrictStr = "tracert -q 1 -f 1 -a {source} {target}"
 
 
@@ -23,7 +23,7 @@ class _IPv6(CommandSet):
     bgp_community: StrictStr = "display bgp ipv6 routing-table community {target} | no-more"
     bgp_aspath: StrictStr = "display bgp ipv6 routing-table regular-expression {target}"
     bgp_route: StrictStr = "display bgp ipv6 routing-table {target} | no-more"
-    ping: StrictStr = "ping ipv6 -c 4 -a {source} {target}"
+    ping: StrictStr = "ping ipv6 -c 5 -a {source} {target}"
     traceroute: StrictStr = "tracert ipv6 -q 1 -f 1 -a {source} {target}"
 
 
@@ -33,7 +33,7 @@ class _VPNIPv4(CommandSet):
     bgp_community: StrictStr = "display bgp vpnv4 vpn-instance {vrf} routing-table community {target} | no-more"
     bgp_aspath: StrictStr = "display bgp vpnv4 vpn-instance {vrf} routing-table regular-expression {target}"
     bgp_route: StrictStr = "display bgp vpnv4 vpn-instance {vrf} routing-table {target} | no-more"
-    ping: StrictStr = "ping -vpn-instance {vrf} -c 4 -a {source} {target}"
+    ping: StrictStr = "ping -vpn-instance {vrf} -c 5 -a {source} {target}"
     traceroute: StrictStr = "tracert -q 1 -f 1 -vpn-instance {vrf} -a {source} {target}"
 
 
@@ -43,8 +43,40 @@ class _VPNIPv6(CommandSet):
     bgp_community: StrictStr = "display bgp vpnv6 vpn-instance {vrf} routing-table community {target} | no-more"
     bgp_aspath: StrictStr = "display bgp vpnv6 vpn-instance {vrf} routing-table regular-expression {target}"
     bgp_route: StrictStr = "display bgp vpnv6 vpn-instance {vrf} routing-table {target} | no-more"
-    ping: StrictStr = "ping vpnv6 vpn-instance {vrf} -c 4 -a {source} {target}"
+    ping: StrictStr = "ping vpnv6 vpn-instance {vrf} -c 5 -a {source} {target}"
     traceroute: StrictStr = "tracert -q 1 -f 1 vpn-instance {vrf} -a {source} {target}"
+
+
+_structured = CommandGroup(
+    ipv4_default=CommandSet(
+        bgp_community="display bgp routing-table community {target} | no-more",
+        bgp_aspath="display bgp routing-table regular-expression {target}",
+        bgp_route="display bgp routing-table {target} | no-more",
+        ping="ping -c 5 -a {source} {target}",
+        traceroute="tracert -q 1 -f 1 -a {source} {target}",
+    ),
+    ipv6_default=CommandSet(
+        bgp_community="display bgp ipv6 routing-table community {target} | no-more",
+        bgp_aspath="display bgp ipv6 routing-table regular-expression {target}",
+        bgp_route="display bgp ipv6 routing-table {target} | no-more",
+        ping="ping ipv6 -c 5 -a {source} {target}",
+        traceroute="tracert ipv6 -q 1 -f 1 -a {source} {target}",
+    ),
+    ipv4_vpn=CommandSet(
+        bgp_community="display bgp vpnv4 vpn-instance {vrf} routing-table community {target} | no-more",
+        bgp_aspath="display bgp vpnv4 vpn-instance {vrf} routing-table regular-expression {target}",
+        bgp_route="display bgp vpnv4 vpn-instance {vrf} routing-table {target} | no-more",
+        ping="ping -vpn-instance {vrf} -c 5 -a {source} {target}",
+        traceroute="tracert -q 1 -f 1 -vpn-instance {vrf} -a {source} {target}",
+    ),
+    ipv6_vpn=CommandSet(
+        bgp_community="display bgp vpnv6 vpn-instance {vrf} routing-table community {target} | no-more",
+        bgp_aspath="display bgp vpnv6 vpn-instance {vrf} routing-table regular-expression {target}",
+        bgp_route="display bgp vpnv6 vpn-instance {vrf} routing-table {target} | no-more",
+        ping="ping vpnv6 vpn-instance {vrf} -c 5 -a {source} {target}",
+        traceroute="tracert -q 1 -f 1 vpn-instance {vrf} -a {source} {target}",
+    ),
+)
 
 
 class HuaweiCommands(CommandGroup):
@@ -54,3 +86,8 @@ class HuaweiCommands(CommandGroup):
     ipv6_default: _IPv6 = _IPv6()
     ipv4_vpn: _VPNIPv4 = _VPNIPv4()
     ipv6_vpn: _VPNIPv6 = _VPNIPv6()
+
+    def __init__(self, **kwargs):
+        """Initialize command group, ensure structured fields are not overridden."""
+        super().__init__(**kwargs)
+        self.structured = _structured
